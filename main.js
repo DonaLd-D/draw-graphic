95936548a53f1ad676522cd5c6a753e8b0605ed0
Donald <1376825153@qq.com>
2020-06-19 16:35:48 +0800

    画布全设置屏全屏

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const app = new PIXI.Application({
   backgroundColor: 0x1099bb,
   antialias: true
 });
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 document.body.appendChild(app.view);
 
 // create a texture from an image path
